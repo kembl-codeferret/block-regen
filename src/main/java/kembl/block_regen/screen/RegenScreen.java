5c1990faae611a9c59288c40f6139009dc6ffcc5
Kendal Minor <123659794+kembl-codeferret@users.noreply.github.com>
2024-05-29 21:36:21 -0500

    Created a menu maybe?
    i have no idea i dont know how to get it to connect to the item

---
+++ src/main/java/kembl/block_regen/screen/RegenScreen.java
@@ -0,0 +1,46 @@
+package kembl.block_regen.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import kembl.block_regen.BlockRegen;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+
+public class RegenScreen extends AbstractContainerScreen<RegenMenu> {
+    private static final ResourceLocation TEXTURE =
+            new ResourceLocation(BlockRegen.MOD_ID, "textures/gui/regen_tool_gui.png");
+
+    public RegenScreen(RegenMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+        super(pMenu, pPlayerInventory, pTitle);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        this.inventoryLabelX = 10000;
+        this.titleLabelX = 10000;
+        this.imageWidth = 230;
+        this.imageHeight = 138;
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+
+        guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        renderBackground(guiGraphics);
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+}
